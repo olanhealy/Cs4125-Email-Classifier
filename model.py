f2e4aa9cc846fa4506eb515c26cd70543d01f5ab
Olan Healy <olanhealy12@gmail.com>
2024-11-11 10:30:18 +0000

    Singleton pattern implemented

--- model.py
+++ model.py
@@ -2,11 +2,13 @@ from src.preprocessing.preprocess import get_processed_data
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.metrics import classification_report
+from utils import config
 
 #THIS IS WHERE THE DATA IS CALLED AND IMPORTED FROM THE PREPROCESSING FUNCTIONS
 train_data, test_data = get_processed_data()
-
-tfidf_vectorizer = TfidfVectorizer(max_features=2000, stop_words='english')
+max_features = config.get("preprocessing.max_features", 2000)
+stop_words = config.get("preprocessing.stop_words", "english")
+tfidf_vectorizer = TfidfVectorizer(max_features=max_features, stop_words=stop_words)
 X_train_vectorized = tfidf_vectorizer.fit_transform(train_data['Interaction content']).toarray()
 X_test_vectorized = tfidf_vectorizer.transform(test_data['Interaction content']).toarray()
 
--- src/models/model_factory.py
+++ src/models/model_factory.py
@@ -3,6 +3,7 @@ from models.naive_bayes import NaiveBayesModel
 from models.xg_boost import XGBoostModel
 from models.svm import SVMModel
 from models.extra_trees import ExtraTreesModel
+from ..utils.config import Configuration
 
 class ClassifierFactory:
     """
@@ -15,21 +16,41 @@ class ClassifierFactory:
         return model_class(**kwargs)
 
     @classmethod
-    def create_naive_bayes_model(cls, alpha=1.0):
+    def create_naive_bayes_model(cls):
+        config = Configuration()
+        alpha = config.get("model_params.naive_bayes.alpha", 1.0)
         return cls.create_model(NaiveBayesModel, alpha=alpha)
 
     @classmethod
-    def create_svm_model(cls, kernel='linear', C=1.0, random_state=0):
+    def create_svm_model(cls):
+        config = Configuration()
+        C = config.get("model_params.svm.C", 1.0)
+        kernel = config.get("model_params.svm.kernel", 'linear')
+        random_state = config.get("model_params.svm.random_state", 0)
         return cls.create_model(SVMModel, kernel=kernel, C=C, random_state=random_state)
 
     @classmethod
-    def create_gradient_boosting_model(cls, n_estimators=100, learning_rate=0.1, random_state=0):
+    def create_gradient_boosting_model(cls):
+        config = Configuration()
+        n_estimators = config.get("model_params.gradient_boosting.n_estimators", 100)
+        learning_rate = config.get("model_params.gradient_boosting.learning_rate", 0.1)
+        random_state = config.get("model_params.gradient_boosting.random_state", 0)
         return cls.create_model(GradientBoostingModel, n_estimators=n_estimators, learning_rate=learning_rate, random_state=random_state)
 
     @classmethod
-    def create_xgboost_model(cls, n_estimators=100, learning_rate=0.1, random_state=0):
-        return cls.create_model(XGBoostModel, n_estimators=n_estimators, learning_rate=learning_rate, random_state=random_state)
+    def create_xgboost_model(cls):
+        config = Configuration()
+        n_estimators = config.get("model_params.xgboost.n_estimators", 100)
+        learning_rate = config.get("model_params.xgboost.learning_rate", 0.1)
+        random_state = config.get("model_params.xgboost.random_state", 0)
+        use_label_encoder = config.get("model_params.xgboost.use_label_encoder", False)
+        eval_metric = config.get("model_params.xgboost.eval_metric", 'mlogloss')
+        return cls.create_model(XGBoostModel, n_estimators=n_estimators, learning_rate=learning_rate, random_state=random_state, use_label_encoder=use_label_encoder, eval_metric=eval_metric)
 
     @classmethod
-    def create_extra_trees_model(cls, n_estimators=1000, random_state=0, class_weight='balanced'):
+    def create_extra_trees_model(cls):
+        config = Configuration()
+        n_estimators = config.get("model_params.extra_trees.n_estimators", 1000)
+        random_state = config.get("model_params.extra_trees.random_state", 0)
+        class_weight = config.get("model_params.extra_trees.class_weight", 'balanced')
         return cls.create_model(ExtraTreesModel, n_estimators=n_estimators, random_state=random_state, class_weight=class_weight)
